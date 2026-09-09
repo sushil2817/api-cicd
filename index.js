9b10990136c116e56d0278a06b156f36b684f1ae
sushil2817 <tony281723@gmail.com>
2026-09-09 11:06:24 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,13 @@
+import express from 'express';
+
+const app = express();
+const PORT = process.env.PORT ?? 8080;
+
+app.get('/',(req,res)=>{
+    return res.json({msg:"Hello From the server"})
+})
+
+
+app.listen(PORT,()=>{
+    console.log(`Server is listening on PORT ${PORT}`);
+})
